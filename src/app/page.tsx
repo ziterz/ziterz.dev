59f92fa0c922812b7af3860148411736035c753b
Ziterz <ziady.mubaraq@gmail.com>
2024-01-21 00:48:15 +0700

    style: navbar desktop and mobile version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, JSX, SVGProps } from "react";
 import { animateScroll } from "react-scroll";
+import Link from "next/link";
 
 export default function Home() {
   const scrollToTop = () => {
@@ -10,6 +11,23 @@ export default function Home() {
   };
   const [opacity, setOpacity] = useState(1);
   const headerRef = useRef<HTMLDivElement>(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    if (isMenuOpen) {
+      setIsMenuOpen(false);
+      document.body.style.overflow = "";
+    } else {
+      setIsMenuOpen(true);
+      document.body.style.overflow = "hidden";
+    }
+  }
+
+  useEffect(() => {
+    return function cleanup() {
+      document.body.style.overflow = "";
+    };
+  }, []);
 
   useEffect(() => {
     const headerHeight = (headerRef.current as HTMLElement)?.clientHeight;
@@ -37,19 +55,63 @@ export default function Home() {
 
   return (
     <>
-      <div className="px-4 md:px-[2%] pt-7 fixed z-30">
-        <header>
-          <nav className="text-2xl py-5 hidden md:block">
+      <header className="px-4 md:px-[2%] pt-7 fixed z-30 w-full">
+        <div className="hidden sm:block z-100">
+          <nav className="text-2xl py-5">
             <div className="inline-block pe-8 active hover:line-through">
-              <a href="">Work</a>
+              <a href="">Portfolio</a>
             </div>
             <div className="inline-block hover:line-through">
               <a href="">About Me</a>
             </div>
           </nav>
-        </header>
-      </div>
-      <div
+        </div>
+        <div className="block sm:hidden z-100">
+          <nav className="text-2xl">
+            <div
+              className={`w-full justify-between flex items-center ${
+                isMenuOpen && "bg-bg"
+              } p-5`}
+              style={{ zIndex: 101 }}
+            >
+              <div></div>
+              <button
+                className="burger visible md:hidden"
+                aria-label="Toggle menu"
+                type="button"
+                onClick={toggleMenu}
+              >
+                <MenuIcon data-hide={isMenuOpen} />
+                <CrossIcon data-hide={!isMenuOpen} />
+              </button>
+            </div>
+            {isMenuOpen && (
+              <ul
+                className={`menu flex flex-col absolute bg-black
+            ${isMenuOpen && "menuRendered"}`}
+              >
+                <li
+                  className="text-gray-100 text-2xl"
+                  style={{ transitionDelay: `${150 + 1 * 25}ms` }}
+                >
+                  <Link href="" className="flex justify-center w-full pb-8 hover:line-through">
+                    Portfolio
+                  </Link>
+                </li>
+                <li
+                  className="text-gray-100 text-2xl"
+                  style={{ transitionDelay: `${150 + 1 * 25}ms` }}
+                >
+                  <Link href="" className="flex justify-center w-full hover:line-through">
+                    About Me
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </nav>
+        </div>
+      </header>
+      <section
         className="px-4 md:px-[2%] fixed mt-8 z-20"
         ref={headerRef}
         style={{ opacity: opacity }}
@@ -59,11 +121,11 @@ export default function Home() {
             Hello! I'm Ziady Mubaraq — Software Engineer, Passionate Educator ↓
           </p>
         </div>
-      </div>
+      </section>
       <div className="placeholder h-[55vh] opacity-0"></div>
-      <div className="md:px-[2%] relative z-20">
+      <section className="md:px-[2%] relative z-20">
         <main>
-          <div className="grid md:grid-cols-2 gap-2">
+          <div className="grid sm:grid-cols-2 gap-2">
             <a className="relative cursor-pointer" href="">
               <div className="relative">
                 <img src="/riot-boardgame.png" />
@@ -136,11 +198,11 @@ export default function Home() {
             </a>
           </div>
         </main>
-      </div>
-      <div className="px-4 md:px-[2%] text-center py-7">
+      </section>
+      <section className="px-4 md:px-[2%] text-center py-7">
         <a
           onClick={scrollToTop}
-          className="fixed bottom-[4%] right-[2%] p-3 z-40 cursor-pointer rounded-full bg-white"
+          className="fixed bottom-[4%] right-[2%] p-3 z-40 cursor-pointer rounded-full bg-white hidden md:block"
         >
           <img src="./arrow.svg" alt="" width={32} />
         </a>
@@ -176,19 +238,65 @@ export default function Home() {
                 </span>
               </div>
             </div>
-            <div className="mt-2 text-md ">
-              Made by{" "}
-              <a
-                href="mailto:ziterz@icloud.com"
-                className="text-gray font-medium"
-              >
-                Ziady Mubaraq
-              </a>
-              . All rights reserved.
-            </div>
           </div>
         </footer>
-      </div>
+      </section>
     </>
+  );
+}
+
+function MenuIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      className="h-10 w-10 absolute text-gray-100"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      {...props}
+    >
+      <path
+        d="M2.5 5.5H17.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2.5 10.5H17.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2.5 15.5H17.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+function CrossIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      className="h-10 w-10 absolute text-gray-100"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="none"
+      shapeRendering="geometricPrecision"
+      {...props}
+    >
+      <path d="M18 6L6 18" />
+      <path d="M6 6l12 12" />
+    </svg>
   );
 }
