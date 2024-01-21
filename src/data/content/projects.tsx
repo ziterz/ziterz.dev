f03dd32702b3564080e08d02f2de53754463a194
Ziterz <ziady.mubaraq@gmail.com>
2024-01-21 17:41:16 +0700

    content: try to duplicate content

--- src/data/content/projects.tsx
+++ src/data/content/projects.tsx
@@ -13,5 +13,18 @@ export const projects: Project[] = [
     github: "https://github.com/braydentw/learn.theyei",
     appstore: "https://github.com/braydentw/learn.theyei",
     testflight: "https://github.com/braydentw/learn.theyei"
+  },
+  {
+    id: 1,
+    path: "riot-boardgame",
+    title: "Riot Hybrid Boardgame",
+    desc: "<span class='text-yellow-400'>Riot Hybrid Boardgame</span> is a hybrid board game with a native iOS companion app that integrates physical and digital elements using NFC technology for interactions.",
+    img: ["/static/riot-boardgame.png", "/static/riot-boardgame.png", "/static/riot-boardgame.png"],
+    tags: ["React", "NextJS", "TailwindCSS", "CSS", "Firebase"],
+    footer: "Role: iOS Developer, UI/UX Designer, Artwork Designer",
+    link: "https://learn.theyei.org/",
+    github: "https://github.com/braydentw/learn.theyei",
+    appstore: "https://github.com/braydentw/learn.theyei",
+    testflight: "https://github.com/braydentw/learn.theyei"
   }
 ];
