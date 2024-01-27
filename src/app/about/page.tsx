b94a758549b05b7c11c8582bd12a1de039097dad
Ziterz <ziady.mubaraq@gmail.com>
2024-01-28 00:59:44 +0700

    content: update content MechKit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -75,6 +75,8 @@ export default function About() {
                 {/* <span className="hidden text-red-500"></span> */}
                 {/* <span className="hidden text-emerald-500"></span> */}
                 {/* <span className="hidden text-neutral-400"></span> */}
+                {/* <span className="hidden text-orange-400"></span> */}
+                {/* <span className="hidden text-cyan-400"></span> */}
                 {/* <span className="hidden text-pink-500 list-inside list-disc"></span> */}
               </p>
             </div>
