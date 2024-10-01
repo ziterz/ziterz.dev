92d58f1baf8339aa4373ec2116d200315ace2bb4
Ziterz <ziady.mubaraq@gmail.com>
2024-10-01 12:37:22 +0700

    copy: update title desc for every page and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,42 @@
-import type { Metadata } from 'next';
-import Head from 'next/head';
+import type { Metadata } from 'next'
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Ziady Mubaraq',
-  description: 'Full-Stack Developer, Coding Mentor',
+  description: 'Full-Stack Engineer, Coding Mentor',
+  applicationName: 'ziterz',
+  authors: [{
+    name: 'Ziady Mubaraq',
+    url: 'https://ziterz.dev/',
+  }],
+  keywords: ['Full-Stack Engineer', 'Coding Mentor', 'HTML', 'CSS', 'Node.js', 'Express.js', 'JavaScript', 'TypeScript', 'Tailwind CSS', 'React','React Native', 'Next.js', 'PostgreSQL', 'MySQL', 'MongoDB', 'Firebase', 'AWS', 'GCP', 'Docker', 'Kubernetes', 'CI/CD', 'Git', 'GitHub', 'Jira', 'Hacktiv8', 'Lunash', 'Apple Developer Academy', 'Jakarta', 'Indonesia'],
+  themeColor: 'dark',
+  viewport: 'width=device-width, initial-scale=1.0',
+  robots: 'index, follow',
+  manifest: {
+    type: "website",
+    url: "https://example.com",
+    title: "My Website",
+    description: "My Website Description",
+    siteName: "My Website",
+    images: [{
+      url: "https://example.com/og.png",
+    }],
+  ,}
+  openGraph: {
+    type: 'website',
+    url: 'https://ziterz.dev/',
+    title: 'Ziady Mubaraq',
+    description: 'Full-Stack Engineer, Coding Mentor',
+    images: [{
+      url: 'https://ziterz.dev/static/ziady.jpg',
+      width: 0,
+      height: 0,
+      alt: 'Ziady Mubaraq',
+    }],
+  },
+  }
+
 };
 
 export default function RootLayout({
@@ -34,14 +66,14 @@ export default function RootLayout({
         />
         <link rel='manifest' href='/favicon/site.webmanifest' />
         <meta name='title' content='Ziady Mubaraq' />
-        <meta name='description' content='Full-Stack Developer, Coding Mentor' />
+        <meta name='description' content='Full-Stack Engineer, Coding Mentor' />
 
         <meta property='og:type' content='website' />
         <meta property='og:url' content='https://ziterz.dev/' />
         <meta property='og:title' content='Ziady Mubaraq' />
         <meta
           property='og:description'
-          content='Full-Stack Developer, Coding Mentor'
+          content='Full-Stack Engineer, Coding Mentor'
         />
         <meta
           property='og:image'
@@ -53,7 +85,7 @@ export default function RootLayout({
         <meta property='twitter:title' content='Ziady Mubaraq' />
         <meta
           property='twitter:description'
-          content='Full-Stack Developer, Coding Mentor'
+          content='Full-Stack Engineer, Coding Mentor'
         />
         <meta
           property='twitter:image'
