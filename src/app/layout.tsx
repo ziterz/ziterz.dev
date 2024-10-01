75192a4756e7d84c22c242599f1e261239afadb7
Ziterz <ziady.mubaraq@gmail.com>
2024-10-01 13:54:49 +0700

    refactor: bracket line code issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,60 +44,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <Head>
-        <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/favicon/apple-touch-icon.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/favicon/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicon/favicon-16x16.png'
-        />
-        <link rel='manifest' href='/favicon/site.webmanifest' />
-        <meta name='title' content='Ziady Mubaraq' />
-        <meta name='description' content='Full-Stack Engineer, Coding Mentor' />
-
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://ziterz.dev/' />
-        <meta property='og:title' content='Ziady Mubaraq' />
-        <meta
-          property='og:description'
-          content='Full-Stack Engineer, Coding Mentor'
-        />
-        <meta
-          property='og:image'
-          content='https://ziterz.dev/static/ziady.jpg'
-        />
-
-        <meta property='twitter:card' content='summary_large_image' />
-        <meta property='twitter:url' content='https://ziterz.dev/' />
-        <meta property='twitter:title' content='Ziady Mubaraq' />
-        <meta
-          property='twitter:description'
-          content='Full-Stack Engineer, Coding Mentor'
-        />
-        <meta
-          property='twitter:image'
-          content='https://ziterz.dev/static/ziady.jpg'
-        ></meta>
-
-        <meta name='theme-color' content='#000000' />
-        <meta name='apple-mobile-web-app-capable' content='yes' />
-        <meta name='apple-mobile-web-app-status-bar-style' content='#07070f' />
-        <meta
-          name='apple-mobile-web-app-status-bar-style'
-          content='black-translucent'
-        />
-      </Head>
       <body>{children}</body>
     </html>
   );
